efa597d3970ec439c7ee802136b7a0cb160afa43
hannytechnica <hannorsmithofficial@gmail.com>
2024-10-04 07:26:04 +0700

    Edited code, added LICENSE.md, and prettify the repository

--- ProstheticArm.ino
+++ ProstheticArm.ino
@@ -1,20 +1,68 @@
-#include <Servo.h>
+// For more information, please visit https://github.com/Lambda-Robotics/mechanical-prosthetic-arm
 
-int emgPin = A0; // Analog pin for EMG
-int emgValue = 0; // EMG Sensor Value
-Servo servo1;
+#define EN        8     // Stepper Motor Switch Pin
+#define X_DIR     5     // X Motor Direction Pin
+#define Y_DIR     6     // Y Motor Direction Pin
+#define X_STP     2     // X Motor Steps Pin
+#define Y_STP     3     // Y Motor Steps Pin
+
+int emgValue = 0; // Variable for EMG
+bool stepperClose = true;
+bool stepperOpen = true;
+
+// Function for stepper motor controlling
+void step(boolean dir, byte dirPin, byte stepperPin, int steps) {
+  digitalWrite(dirPin, dir);
+  delay(50);
+  for (int i = 0; i < steps; i++) {  
+    digitalWrite(stepperPin, HIGH);
+    delayMicroseconds(800);        
+    digitalWrite(stepperPin, LOW);
+    delayMicroseconds(800);       
+  }
+}
+// Same function, but for reverse direction
+void stepreverse(boolean dir, byte dirPin, byte stepperPin, int steps) {
+  digitalWrite(dirPin, !dir);
+  delay(50);
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(stepperPin, HIGH);
+    delayMicroseconds(800);        
+    digitalWrite(stepperPin, LOW); 
+    delayMicroseconds(800);        
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // Define motor pins as output
+  pinMode(X_DIR, OUTPUT); pinMode(X_STP, OUTPUT);
+  pinMode(Y_DIR, OUTPUT); pinMode(Y_STP, OUTPUT);
+  pinMode(EN, OUTPUT);
+
+  // Enable the motors
+  digitalWrite(EN, LOW);
+
+  // Enable Serial Monitor
   Serial.begin(9600);
-  servo1.attach(6);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  emgValue = analogRead(emgPin);
-  int servo1POS = map(emgValue, 0, 1023, 0, 180);
-  servo1.write(servo1POS);
+  emgValue = analogRead(A8);
   Serial.println(emgValue);
-  delay(50);
+
+  // Check conditions
+  if (emgValue < 240) {
+    stepperClose = true;
+    if (stepperOpen) {
+      step(true, X_DIR, X_STP, 200);
+      stepperOpen = false;
+    }
+  } else {
+    stepperOpen = true;
+    if (stepperClose) {
+      stepreverse(true, X_DIR, X_STP, 200);
+      stepperClose = false;
+    }
+  }
+  delay(1000);
 }
--- demo/mpaDemo.ino
+++ demo/mpaDemo.ino
@@ -1,77 +1,70 @@
-#define EN        8     // ขาเปิดใช้งานมอเตอร์สเต็ปเปอร์ (Active Low)
-#define X_DIR     5     // ขาควบคุมทิศทางมอเตอร์แกน X
-#define Y_DIR     6     // ขาควบคุมทิศทางมอเตอร์แกน Y
-#define X_STP     2     // ขาควบคุมสเต็ปมอเตอร์แกน X
-#define Y_STP     3     // ขาควบคุมสเต็ปมอเตอร์แกน Y
+// The demo file was made for demonstration, using potentiometer for switching between the act of closing and opening of palm, with only one stepper working.The "demo" directory was made for demonstration, using potentiometer for switching between the act of closing and opening of palm, with only one stepper working.
+// For more information, please visit https://github.com/Lambda-Robotics/mechanical-prosthetic-arm
 
-int r = 0; // ตัวแปรสำหรับเก็บค่าที่อ่านได้จาก potentiometer
+#define EN        8     // Stepper Motor Switch Pin
+#define X_DIR     5     // X Motor Direction Pin
+#define Y_DIR     6     // Y Motor Direction Pin
+#define X_STP     2     // X Motor Steps Pin
+#define Y_STP     3     // Y Motor Steps Pin
+
+int r = 0; // Variable for Potentiometer
 bool stepperClose = true;
 bool stepperOpen = true;
 
-// ฟังก์ชัน: step
-// หน้าที่: ควบคุมทิศทางและจำนวนสเต็ปของมอเตอร์สเต็ปเปอร์
-// พารามิเตอร์: 
-//   dir - ควบคุมทิศทาง (boolean)
-//   dirPin - ขาทิศทางของมอเตอร์สเต็ปเปอร์ที่ต้องการควบคุม
-//   stepperPin - ขาสเต็ปของมอเตอร์สเต็ปเปอร์
-//   steps - จำนวนสเต็ปที่ต้องการให้มอเตอร์หมุน
+// Function for stepper motor controlling
 void step(boolean dir, byte dirPin, byte stepperPin, int steps) {
-  digitalWrite(dirPin, dir); // กำหนดทิศทาง
+  digitalWrite(dirPin, dir);
   delay(50);
-  for (int i = 0; i < steps; i++) {  // วนลูปตามจำนวนสเต็ป
-    digitalWrite(stepperPin, HIGH);  // ส่งสัญญาณสูงไปที่ขาสเต็ป
-    delayMicroseconds(800);          // หน่วงเวลา 800 ไมโครวินาที
-    digitalWrite(stepperPin, LOW);   // ส่งสัญญาณต่ำไปที่ขาสเต็ป
-    delayMicroseconds(800);          // หน่วงเวลา 800 ไมโครวินาที
+  for (int i = 0; i < steps; i++) {  
+    digitalWrite(stepperPin, HIGH);
+    delayMicroseconds(800);        
+    digitalWrite(stepperPin, LOW);
+    delayMicroseconds(800);       
   }
 }
-
+// Same function, but for reverse direction
 void stepreverse(boolean dir, byte dirPin, byte stepperPin, int steps) {
-  digitalWrite(dirPin, !dir); // กำหนดทิศทาง
+  digitalWrite(dirPin, !dir);
   delay(50);
-  for (int i = 0; i < steps; i++) {  // วนลูปตามจำนวนสเต็ป
-    digitalWrite(stepperPin, HIGH);  // ส่งสัญญาณสูงไปที่ขาสเต็ป
-    delayMicroseconds(800);          // หน่วงเวลา 800 ไมโครวินาที
-    digitalWrite(stepperPin, LOW);   // ส่งสัญญาณต่ำไปที่ขาสเต็ป
-    delayMicroseconds(800);          // หน่วงเวลา 800 ไมโครวินาที
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(stepperPin, HIGH);
+    delayMicroseconds(800);        
+    digitalWrite(stepperPin, LOW); 
+    delayMicroseconds(800);        
   }
 }
 
 void setup() {
-  // กำหนดให้ขาที่ใช้กับมอเตอร์เป็นเอาท์พุต
+  // Define motor pins as output
   pinMode(X_DIR, OUTPUT); pinMode(X_STP, OUTPUT);
   pinMode(Y_DIR, OUTPUT); pinMode(Y_STP, OUTPUT);
   pinMode(EN, OUTPUT);
 
-  // เปิดใช้งานมอเตอร์สเต็ปเปอร์
+  // Enable the motors
   digitalWrite(EN, LOW);
 
-  // เริ่มต้นการทำงานของ Serial Monitor
+  // Enable Serial Monitor
   Serial.begin(9600);
 }
 
 void loop() {
-  // อ่านค่าจากตัวต้านทานปรับค่า (Potentiometer) ที่ขา A03
-  r = analogRead(A8);  // อ่านค่าจากขา A3 (0 - 1023)
+  r = analogRead(A8);
 
-  // แสดงค่าที่อ่านได้ทาง Serial Monitor
   Serial.println(r);
 
-  // ตรวจสอบเงื่อนไข
+  // Check conditions
   if (r > 500) {
     stepperClose = true;
     if (stepperOpen) {
-      step(true, X_DIR, X_STP, 100);
+      step(true, X_DIR, X_STP, 200);
       stepperOpen = false;
     }
-    // step(true, X_DIR, X_STP, 200);
   } else {
     stepperOpen = true;
     if (stepperClose) {
-      stepreverse(true, X_DIR, X_STP, 100);
+      stepreverse(true, X_DIR, X_STP, 200);
       stepperClose = false;
     }
-    // step(false, X_DIR, X_STP, 200);
   }
-  delay(1000);  // หน่วงเวลา 1 วินาที
+  delay(1000);
 }
